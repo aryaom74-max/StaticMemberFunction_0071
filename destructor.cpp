bb37a58517c40f9383f2994a221384f37b79f09e
Muh Arya Saputra <aryaom74@gmail.com>
2026-05-05 18:00:58 +0700

    Update destructor.cpp

--- destructor.cpp
+++ destructor.cpp
@@ -6,4 +6,16 @@ class angka
 private:
     int *arr;
     int panjang;
+public:
+    angka(int); // Constructor
+    ~angka();   // Destructor
+    void cetakData();
+    void isiData();
+};
+
+angka::angka(int i){ // Constructor
+    panjang = i;
+    arr = new int[i];
+    isiData();
+}
 
