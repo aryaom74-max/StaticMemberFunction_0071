48a3bb92e489396781fe9d0a251dd203081f684a
Muh Arya Saputra <aryaom74@gmail.com>
2026-05-05 17:58:06 +0700

    Update chalenge.cpp

--- chalenge.cpp
+++ chalenge.cpp
@@ -1,2 +1,30 @@
 #include <iostream>
 #include <string>
+
+using namespace std;
+
+class Produk {
+private:
+    int id;
+    string nama;
+
+public:
+    // 1. Constructor tanpa parameter (nilai default)
+    Produk() {
+        id = 0;
+        nama = "Belum ada nama";
+    }
+
+    // 2. Constructor dengan parameter id saja
+    Produk(int pId) {
+        id = pId;
+        nama = "Tanpa Nama";
+    }
+
+    // 3. Constructor dengan parameter nama saja
+    Produk(string pNama) {
+        id = 0;
+        nama = pNama;
+    }
+
+   
