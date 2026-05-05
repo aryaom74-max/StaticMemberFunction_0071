51356e0878924e57788fa22fbea798bdc7697f76
Muh Arya Saputra <aryaom74@gmail.com>
2026-05-05 17:56:16 +0700

    Add getJudul method and demonstrate chaining

--- chainfunctioncall.cpp
+++ chainfunctioncall.cpp
@@ -10,3 +10,16 @@ class buku{
         this->judul = judul;
         return *this;   //chain function
     }
+        string getJudul(){
+        return this->judul;
+    }
+};
+
+int main()
+{
+    buku bukunya;
+    //bukunya.setJudul("Matematika");
+    //cout << bukunya.getJudul()<< endl;
+    cout<<bukunya.setJudul("Matematika").getJudul();    //chain function calls
+    return 0;
+}
